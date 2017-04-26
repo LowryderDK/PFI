e78ec29c34e99fec3c4ab2b82e85ff4a01ace0dc
VickyPoul <vickypoulsen@hotmail.com>
2017-04-26 22:15:58 +0200

    Same name change in the Point class

--- Snake Project 02/src/snake02/Point.java
+++ Snake Project 02/src/snake02/Point.java
@@ -7,41 +7,41 @@ package snake02;
 public class Point 
 {
 	
-	private int x, y;
+	private int xPos, yPos;
 	
 	//Default Point constructor
 	public Point() 
 	{
-		x = 0;
-		y = 0;
+		xPos = 0;
+		yPos = 0;
 	}
 	
 	//Additional Point constructor
-	public Point(int x, int y)
+	public Point(int xPos, int yPos)
 	{
 		//assigning to instance variables
-		this.x = x;
-		this.y = y;
+		this.xPos = xPos;
+		this.yPos = yPos;
 	}
 	
 	//creating access
-	public void setX(int x)
+	public void setX(int xPos)
 	{
-		this.x = x;
+		this.xPos = xPos;
 	}
 	
-	public void setY(int y)
+	public void setY(int yPos)
 	{
-		this.y = y;
+		this.yPos = yPos;
 	}
 	
 	public int getX()
 	{
-		return x;
+		return xPos;
 	}
 	
 	public int getY()
 	{
-		return y;
+		return yPos;
 	}
 }
